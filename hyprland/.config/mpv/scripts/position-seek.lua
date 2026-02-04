bb59005ec276ed517ae46d4d2c15a8346f7d3930
Samin <adnanbr786@gmail.com>
2026-02-05 02:24:54 +0600

    sync: Thu Feb  5 02:24:54 AM +06 2026

--- hyprland/.config/mpv/scripts/position-seek.lua
+++ hyprland/.config/mpv/scripts/position-seek.lua
@@ -9,10 +9,10 @@ function seek_based_on_position()
 
     if x > 0.85 then          -- only extreme right (last 15%) → seek forward
         mp.commandv("seek", 10)
-        mp.osd_message("→ +10s")
+        mp.osd_message("+10s >")
     elseif x < 0.15 then      -- only extreme left (first 15%) → seek backward
         mp.commandv("seek", -10)
-        mp.osd_message("← -10s")
+        mp.osd_message("< -10s")
     else                      -- everything else (central 70%) → fullscreen toggle
         mp.commandv("cycle", "fullscreen")
     end
